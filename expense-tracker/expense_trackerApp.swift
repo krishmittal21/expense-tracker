56d89dce6c475c2858319debf384cb363f5e7caa
Krish Mittal <krishmittal212005@gmail.com>
2024-10-30 12:09:46 +0530

    Initial Commit

---
+++ expense-tracker/expense_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_trackerApp.swift
+//  expense-tracker
+//
+//  Created by Krish Mittal on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct expense_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
